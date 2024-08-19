92db8f0e3dfee629b81b16fc409b9c4a2a55885d
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-08-19 06:34:12 -0300

    Diseño del NAV
    Terminé la parte estética del NAV, incluyendo solo el CSS y el HTML.

--- reactjs/src/Componentes/Nav/Nav.jsx
+++ reactjs/src/Componentes/Nav/Nav.jsx
@@ -1,5 +1,22 @@
 function Nav() {
-  return <h1>J.Leonardo.N</h1>;
+  return (
+    <>
+      <nav className="nav">
+        <div className="navTamaño">
+
+        {/* Titulo  */}
+        <h1 className="navTitulo">
+          J.<span className="navSpan">Leo</span>nardo.N
+        </h1>
+        {/* Enlaces */}
+        <ul className="lista enlacesUl">
+          <li className="enlacesLi">Contador</li>
+          <li className="enlacesLi">Nombres</li>
+        </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Nav;
